0cf9f86c837a21d4616ec769d8371d21f7cc9add
Loulaan <ivan.kononihin@gmail.com>
2022-05-22 16:05:15 +0300

    * Clean code * Add system and example of usage * Edit report (master thesis style and title page) * Add description of files in README.md

--- System/main.py
+++ System/main.py
@@ -4,9 +4,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from utils.hmatr import Hmatr
-from ThreshDetection.thresh import ThreshAnalytical
+from System.thresh import ThreshAnalytical
 from utils.utils import generate_series, find_Q_hat
 
+
+class System:
+    """
+    Система для обнаружения неоднородности. Выводим момент нарушения однородности.
+    """
+    def __init__(self):
+        self.F = None
+        self.k = None
+        self.w1 = None
+        self.delta_min = None
+        self.w_min = None
+        self.sigma_sq = None
+
+        self.initial_value = None
+        self.B = None
+        self.T = None
+        self.L = None
+        self.thresh = None
+
+        self.row = None
+        self.Q_hat = None
+
+    def __call__(self, *args, **kwargs):
+        self.F = kwargs.get('F')
+        self.k = kwargs.get('k')
+        self.w1 = kwargs.get('w1')
+        self.delta_min = kwargs.get('delta_min')
+        self.sigma_sq = kwargs.get('sigma_sq')
+
+        self.w_min = self.w1 + self.delta_min
+        self.initial_value = self.sigma_sq / (0.5 + self.sigma_sq)
+        self.B = int(len(self.F) / 10)
+        self.T = self.B
+        self.L = int(0.4 * self.T)
+        self.thresh = ThreshAnalytical(omega_1=self.w1, omega_2=self.w_min, L=self.L, T_=self.T, k=self.k,
+                                       initial_value=self.initial_value)
+        self.row = Hmatr(f=self.F, B=self.B, T=self.T, L=self.L, neig=2, svdMethod='svd').getRow()
+        self.Q_hat = find_Q_hat(self.row, self.thresh.thresh)
+        return self.Q_hat + self.T
 
 def test():
     N = 700  # Длина ряда
@@ -84,5 +123,20 @@ def test():
         plt.show()
 
 
+def main():
+    N = 700  # Длина ряда
+    w1 = 1 / 10  # Начальная частота
+    delta_min = 1 / 15  # Минимальная разница в частотах для обнаружения неоднородности
+    k = 30  # Кол-во точек, за которые нужно обнаружить разладку
+    w2 = 1/8
+    Q = 301
+
+    original_series = generate_series(w1, w2, Q, N) + np.random.normal(scale=0.5, size=N)
+    system = System()
+    Q_hat = system(F=original_series, k=k, w1=w1, delta_min=delta_min, sigma_sq=0.25)
+    print(Q_hat)
+
+
 if __name__ == "__main__":
-    test()
+    main()
+    # test()
